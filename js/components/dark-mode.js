8d7b15e71a05315eed47c17019f832f690409372
Erika <erikaestudosaraujo@gmail.com>
2025-02-26 08:41:31 -0300

    modificando botão dark light

--- js/components/dark-mode.js
+++ js/components/dark-mode.js
@@ -1,22 +1,7 @@
-const btnDark = document.querySelector(".btn-dark");
+const boxDarkLight = document.getElementById("box-dark_light");
 const body = document.querySelector("body");
-const textos = document.querySelectorAll(".boxTexto p");
-const titulos = document.querySelectorAll(".title-section");
-const email = document.querySelector(".arroba");
 
-btnDark.addEventListener("click", () => {
-  btnDark.classList.toggle("btn-dark_active");
-  body.classList.toggle("body-dark_active");
-
-  textos.forEach((p) => {
-    p.style.color = p.style.color === "white" ? "black" : "white";
-  });
-
-  titulos.forEach((titulo) => {
-    titulo.style.color =
-      titulo.style.color === "goldenrod" ? "black" : "goldenrod";
-  });
-  if (email) {
-    email.style.color = email.style.color === "white" ? "black" : "white";
-  }
+boxDarkLight.addEventListener("click", () => {
+  boxDarkLight.classList.toggle("dark");
+  body.classList.toggle("dark");
 });
