7c347277145bd6736cac5e0cd10afa4d5238b951
Erika <erikaestudosaraujo@gmail.com>
2025-06-17 14:13:22 -0300

    Ajuste no responsivo, retirada de preload

--- js/components/preload.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("DOMContentLoaded", () => {
-  const preloader = document.querySelector(".preloader");
-
-  window.addEventListener("load", () => {
-    setTimeout(() => {
-      preloader.classList.add("fade-out");
-    }, 500); // tempo de espera para remover o preloader (em milissegundos)
-  });
-});
